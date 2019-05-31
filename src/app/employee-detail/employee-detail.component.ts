1b6393bc75ccda9e32933e48dc3fd3251b5d037f
aayush kc <kcaayush2158@gmail.com>
2019-05-31 17:38:23 +0545

    Project Uploaded

---
+++ src/app/employee-detail/employee-detail.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../employee.service';
+
+@Component({
+  selector: 'app-employee-detail',
+  template: '<hr><h2>Employee List Detail </h2><br><ul *ngFor="let employee of Employees"><li>{{employee.id}}  {{employee.name}}  {{employee.Age}}</ul>',
+  styleUrls: ['./employee-detail.component.css']
+})
+export class EmployeeDetailComponent implements OnInit {
+
+  public Employees =[];
+
+  
+  constructor(private _employeeService : EmployeeService) { }
+
+  ngOnInit() {
+    this._employeeService.getEmployees().subscribe(data => this.Employees = data);
+  }
+
+}
